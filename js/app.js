2feadc8963e12d79aa97ed2ed6e02af63b3ea681
masudtalukdar <masudtalukdar.cn@gmail.com>
2021-09-13 01:53:12 +0800

    Fix the bug(Image, Image row, Remove Dollar sign)

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ const loadProducts = () => {
 };
 loadProducts();
 
-// show all product in UI 
+// show all product in UI
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
+    const div = document.createElement('div');
+    div.classList.add('product');
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
@@ -23,16 +23,16 @@ const showProducts = (products) => {
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
-    document.getElementById("all-products").appendChild(div);
+    document.getElementById('all-products').appendChild(div);
   }
 };
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
-  updatePrice("price", price);
+  updatePrice('price', price);
 
   updateTaxAndCharge();
-  document.getElementById("total-Products").innerText = count;
+  document.getElementById('total-Products').innerText = count;
 };
 
 const getInputValue = (id) => {
@@ -56,25 +56,26 @@ const setInnerText = (id, value) => {
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
-  const priceConverted = getInputValue("price");
+  const priceConverted = getInputValue('price');
   if (priceConverted > 200) {
-    setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.2);
+    setInnerText('delivery-charge', 30);
+    setInnerText('total-tax', priceConverted * 0.2);
   }
   if (priceConverted > 400) {
-    setInnerText("delivery-charge", 50);
-    setInnerText("total-tax", priceConverted * 0.3);
+    setInnerText('delivery-charge', 50);
+    setInnerText('total-tax', priceConverted * 0.3);
   }
   if (priceConverted > 500) {
-    setInnerText("delivery-charge", 60);
-    setInnerText("total-tax", priceConverted * 0.4);
+    setInnerText('delivery-charge', 60);
+    setInnerText('total-tax', priceConverted * 0.4);
   }
 };
 
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+    getInputValue('price') +
+    getInputValue('delivery-charge') +
+    getInputValue('total-tax');
+  document.getElementById('total').innerText = grandTotal;
 };
