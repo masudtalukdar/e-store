d3466e5e7355d5c78733616432ef68e8f18e90dc
masudtalukdar <masudtalukdar.cn@gmail.com>
2021-09-13 14:39:01 +0800

    bug fixed and added styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const loadProducts = () => {
+  // const url = `https://fakestoreapi.com/products`;
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
@@ -10,15 +11,17 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement('div');
     div.classList.add('product');
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${product.image}></img>
+    <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p>Total-Rating: ${product.rating.count}</p>
+            <p>Avarage-Rating: ${product.rating.rate}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -33,6 +36,7 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById('total-Products').innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
@@ -46,12 +50,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
